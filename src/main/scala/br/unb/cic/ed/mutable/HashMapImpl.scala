b7af85fc0c0beaa76448fd28356eb6b91c029b3b
Zigfreud <thalesmenezes13@gmail.com>
2018-04-25 22:18:59 -0300

    Tratamento de colisões e adapções nas outras funções para tal

--- src/main/scala/br/unb/cic/ed/mutable/HashMapImpl.scala
+++ src/main/scala/br/unb/cic/ed/mutable/HashMapImpl.scala
@@ -7,34 +7,104 @@ package br.unb.cic.ed.mutable
   * @author: thaleslim
   */
 
-case class HashMapElement[A, B](var key: A, var value: B)
+case class HashMapElement[A <: Comparable[A], B](var key: A, var value: B){
+	def equalTo (key: A): Boolean = if( key.compareTo(this.key) == 0 ) true else false
+}
 
 class ArrayHashMap[A <: Comparable[A], B: Manifest](private val max: Int = 10) extends HashMap[A,B]{
 
     private var elements = Array.ofDim[HashMapElement[A,B]](max)
 	private var default_hash: A => Int = ( _.hashCode() % max )
 
-	private def get_position(key: A): Int = {
+	private def sondagem_quadratica(start: Int): Int = {
+		var index = start
+		var contador = 1
 		
+		while( index < max && this.elements(index) != null )
+			index = index + 2 * contador + 5 * contador * contador
+		
+		if( index < max && this.elements(index) == null ) index else -1
+	}
+
+	private def sondagem_linear(start: Int): Int = {
+		var index = start
+		var found: Boolean = false
+
+		var break: Boolean = false
+		while (!break){
+			index += 1
+
+			if( !(index < max) && start != 0 )
+				index = 0
+			else if( this.elements(index) == null ){
+				break = true; found = true
+			}else
+				break = true
+
+		}
+		
+		if( found ) return index else return -1
+	}
+
+	private def get_position(key: A): Int = {
+		var index: Int = this.default_hash(key) % max
+		
+		if( index > -1 && index < max && this.elements(index) == null )
+			return index
+		
+		var position = sondagem_quadratica(index)
+
+		if( position > -1 )
+			return position
+		
+		position = sondagem_linear(index)
+
+		if( position > -1 )
+			return position
+		
+		return -1
 	}
 
 	private def insert(key: A, value: B): Unit = {
-		var index = this.default_hash(key)
-		if(index > -1 && index < max ){
-			if(this.elements(index) == null){
-				this.elements(index) = HashMapElement[A,B](key,value)
-			}
-			else throw br.unb.cic.ed.mutable.InvalidArgument("Position occupied");
-		}
-		else throw br.unb.cic.ed.mutable.InvalidArgument("Out of range");
+		var index = get_position(key)
+		if( index >= 0 )
+			this.elements(index) = HashMapElement[A,B](key,value)
+		else throw br.unb.cic.ed.mutable.InvalidArgument("Maximum Capacity Reached")
     }
+
+	private def locate(start: Int, key: A): Int = {
+		var index = start
+		var found: Boolean = false
+
+		var break: Boolean = false
+		while( !break ){
+			index += 1
+			
+			if( !(index < max) )
+				index = 0
+			else if( index == start )
+				break = true
+			else if( this.elements(index) != null && this.elements(index).equalTo(key) ){
+				break = true; found = true
+			}
+
+		}
+
+		if( found ) return index else return -1
+	}
 	
 	private def search(key: A): Option[B] = {
         if( max == 0 ) None
-		var index = this.default_hash(key)
+		var index = this.default_hash(key) % max
 		if( index > -1 && index < max && this.elements(index) != null ){
-			if( key.compareTo(this.elements(index).key) == 0 ){
+			if( this.elements(index).equalTo(key) )
 				return Some(this.elements(index).value)
+			else{
+				index = locate(index,key)
+				if( index > -1 )
+					return Some(this.elements(index).value)
+				else
+					return None
 			}
 		}
 		return None
@@ -54,9 +124,16 @@ class ArrayHashMap[A <: Comparable[A], B: Manifest](private val max: Int = 10) e
 	}
 
 	def - (key: A): Unit = {
-		var index = this.default_hash(key) 
-		if(max > 0 && index > -1 && index < max && this.elements(index).key == key)
-			this.elements(index) = null
+		var index = this.default_hash(key) % max
+		if( max > 0 && index > -1 && index < max && this.elements(index) != null ){
+			if( this.elements(index).equalTo(key) )
+				this.elements(index) = null
+			else{
+				index = locate(index, key)
+				if( index > -1 )
+					this.elements(index) = null
+			}
+		}	
 	}
 
 }
--- src/test/scala/br/unb/cic/ed/mutable/TestHashMap.scala
+++ src/test/scala/br/unb/cic/ed/mutable/TestHashMap.scala
@@ -12,7 +12,7 @@ class TestHashMap extends FlatSpec with Matchers with GivenWhenThen with BeforeA
   var hashMap: br.unb.cic.ed.mutable.HashMap[String,String] = _ 
 
   before {
-    hashMap = new br.unb.cic.ed.mutable.ArrayHashMap[String,String]()
+    hashMap = new br.unb.cic.ed.mutable.ArrayHashMap[String,String](4)
 	hashMap(_.length())
   }
 
@@ -23,18 +23,40 @@ class TestHashMap extends FlatSpec with Matchers with GivenWhenThen with BeforeA
 
   it should "return None after calling hashMap(\"two\") na estrutura hashMap(\"one\" -> \"um\")" in {
 	hashMap("one" -> "um")
-	hashMap("two'") should be (None)
+	hashMap("two") should be (None)
   }
 
-  it should "return None after calling hashMap(\"three\") na estrutura hashMap(\"one\" -> \"um\", \"2two\" -> \"dois\")" in {
+  it should "return None after calling hashMap(\"three\") na estrutura hashMap(\"one\" -> \"um\", \"two\" -> \"dois\")" in {
 	hashMap("one" -> "um", "two" -> "dois")
 	hashMap("three") should be (None)
+  }
+
+  it should "be able to retrive every value from hashMap(\"1one\" -> \"um\", \"2two\" -> \"dois\", \"three\" -> \"três\", \"four\" -> \"quatro\")" in {
+	hashMap("one" -> "um", "two" -> "dois", "three" -> "três", "four" -> "quatro")
+	hashMap("one") should be (Some("um"))
+	hashMap("two") should be (Some("dois"))
+	hashMap("three") should be (Some("três"))
+	hashMap("four") should be (Some("quatro"))
   }
 
   it should "remove the element (\"um\") after executing hashMap - (\"one\")" in {
 	hashMap("one" -> "um")
 	hashMap - ("one")
 	hashMap("one") should be (None)
+  }
+
+  it should "explode if you try to insert more elements than previously informed" in {
+	hashMap("one" -> "um", "two" -> "dois", "three" -> "três", "four" -> "quatro")
+	
+	hashMap("one") should be (Some("um"))
+	hashMap("two") should be (Some("dois"))
+	hashMap("three") should be (Some("três"))
+	hashMap("four") should be (Some("quatro"))
+
+	intercept[InvalidArgument] {
+		hashMap("five" -> "cinco")
+	}
+
   }
 
 }
