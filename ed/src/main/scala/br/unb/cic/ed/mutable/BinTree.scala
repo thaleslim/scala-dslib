9c5202397953063644a65606a361248f675b65aa
Zigfreud <thalesmenezes13@gmail.com>
2018-04-27 23:20:49 -0300

    Limpeza de duplicata

--- ed/src/main/scala/br/unb/cic/ed/mutable/BinTree.scala
+++
@@ -1,6 +0,0 @@
-package br.unb.cic.ed.mutable
-
-trait BinTree[T <: Comparable[T]] {
-  def insert(value : T): Unit
-  def exist(value: T): Boolean
-}
--- ed/src/main/scala/br/unb/cic/ed/mutable/Exceptions.scala
+++
@@ -1,6 +0,0 @@
-package br.unb.cic.ed.mutable
-
-
-final case class InvalidArgument(private val message: String = "",
-  private val cause: Throwable = None.orNull) extends Exception(message, cause)
-
