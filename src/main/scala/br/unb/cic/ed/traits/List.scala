e9e48574ea9cf1d3a3b8ec5e78e19c82a3949a25
Zigfreud <thalesmenezes13@gmail.com>
2018-05-05 16:29:51 -0300

    Documentação do trait list conforme scaladoc

--- src/main/scala/br/unb/cic/ed/traits/List.scala
+++ src/main/scala/br/unb/cic/ed/traits/List.scala
@@ -1,119 +1,168 @@
 package br.unb.cic.ed.traits
 
 /**
-  * Uma especificação do tipo lista usando
-  * a construção trait da linguagem Scala. 
+  * The definition of a List is based upon a structure that 
   * 
   * @author rbonifacio / thaleslim
   */
 
-/*
- Inserção:
-    list.insert(0,5)
-    list(5)
-        ou uma sequência de valores list(5,6,7)
-    list(0 -> 5) ou list((0,5))
-    list + 5
-    list + (0,5)
-        ou uma sequência list + ((0,5),(1,6),(2,7))
-
- Remoção:
-    list.remove(0)
-    list - (5)
-        ou uma sequência list - (5,6,7)
-*/
-//TODO: comentar & usar comando doc ( generated in </home/thaleslim/Área de Trabalho/scala-dslib/target/scala-2.12/api> )
 trait List[T] extends Traversable[T]{
     /**
-      * Insere um valor em uma posição
-      * @example 
-      * {{{ var list: List[Int]
+      * Inserts a value in this
+      * @param index value's position within this
+      * @example {{{
+      *      var list: List[Int]
       *      list.insert(0,5)    //list = [5]
       * }}}
       */
-    def insert        (idx: Int, value: T) : Unit
+    def insert        (index: Int, value: T) : Unit
     /**
-      * Remove o valor presente em uma posição
-      * @example 
-      * {{{ var list: List[Int]
+      * Removes a value from a position
+      * @param index value's position within this
+      * @example {{{
+      *      var list: List[Int]
       *      list(1,2,3,4,5)     //list = [1,2,3,4,5]
       *      list.remove(2)      //list = [1,2,4,5]
       * }}}
       */       
-    def remove        (idx: Int)           : Unit
+    def remove        (index: Int)           : Unit
     /**
-      * Recebe um índice e retorna o valor da posição correspondente
-      * @return Some(valor) ou None, caso não encontre valor
-      * @example 
-      * {{{ var list: List[Int]
+      * Based on a index, returns a value from within this
+      * @param index value's position within this
+      * @example {{{
+      *      var list: List[Int]
       *      list(1,2,3,4,5)     //list = [1,2,3,4,5]
       *      list.elementAt(2)   //== Some(3)
       *      list.elementAt(7)   //== None
       * }}}
+      * @return Some(value) or None, if value doesn't exist within this
       */
-    def elementAt     (idx: Int)           : Option[T]
+    def elementAt     (index: Int)           : Option[T]
     /**
-      * Busca pela posição correspondente a um certo valor 
-      * @return Some(posição) ou None, caso não encontre valor
-      * @example 
-      * {{{ var list: List[Int]
+      * Searchs for a value and returns its position within this
+      * @example {{{
+      *      var list: List[Int]
       *      list(1,2,3,4,5)     //list = [1,2,3,4,5]
       *      list.find(2)        //== Some(1)
       *      list.find(88)       //== None
       * }}}
+      * @return Some(index) or None, if value doesn't exist within this 
       */
     def find          (value: T)           : Option[Int]
     /**
-      * Concatena this e that
-      * @example 
-      * {{{ var list: List[Int]; var that: List[Int]
+      * Concatenates this & that, without modifying that
+      * @example {{{
+      *      var list: List[Int]; var that: List[Int]
       *      list(1,2);that(3,4) //list = [1,2]     && that = [3,4]
       *      list.addAll(that)   //list = [1,2,3,4] && that = [3,4]
       * }}}
       */
     def addAll[B <: T](that: List[B])      : Unit
-    /** Remove todos os elementos */
+    /**
+      * Removes all of this' elements
+      * @example {{{
+      *      var list: List[Int]
+      *      list(1,2,3,4,5)     //list = [1,2,3,4]
+      *      list.clear          //list = []
+      * }}}
+      */
     def clear(): Unit
-
-    /** Substitue os elementos de this pelos de that */
+    /**
+      * Substitutes this' elements for a copy of that' elements
+      * @example {{{
+      *      var list: List[Int]; var listAux: List[Int]
+      *      list(1,2,3)         //list = [1,2,3]
+      *      listAux(10,20,30)   //listAux = [10,20,30]
+      *      list.subst(listAux) //list = [10,20,30] 
+      * }}}
+      */
     def subst (that: List[T]) : Unit = { 
         this.clear
         this.addAll(that)
     }
-    
     /**
-      * Insere o valor tuple._2 na posição tuple._1
-      * @example 
-      * {{{ var list: List[Int]
+      * Extracts from a tuple the args to insert a value
+      * @param tuple will be used as in this.insert(tuple._1,tuple._2)
+      * @example {{{
+      *      var list: List[Int]
       *      list(0 -> 2)        //== list((0,2)) = [2]
       * }}}
+      * @see [[List!.insert(index:Int,value:T):Unit* this.insert(Int,T)]]
       */
     def apply (tuple: Tuple2[Int,T]) = this.insert(tuple._1,tuple._2)
-    /** Mesmo comportamento de this([[List!.apply(tuple:(Int,T)):Unit* Tuple2[Int,T]]]) */
+    /** 
+      * Adds a tuple2[Int,T] to this
+      * @example {{{
+      *      var list: List[Int]
+      *      list + (0 -> 2)     //== list + ((0,2)) = [2]
+      * }}}
+      * @see [[List!.apply(tuple:(Int,T)):Unit* this( (Int,T) )]]
+      */
     def +     (tuple: Tuple2[Int,T]) = this(tuple)
-    /**Insere a sequência de tuplas na lista de acordo com ._1 de cada elemento*/
+    /**
+      * Adds a tuple2[Int,T] sequence to this
+      * @example {{{
+      *      var list: List[Int]
+      *      list + (0->2, 1->3, 2->4)
+      *      //== list + ((0,2),(1,3),(2,4)) = [2,3,4]
+      * }}}
+      * @see [[List!.+(tuple:(Int,T))* this( (Int,T) )]] [[https://www.scala-lang.org/api/2.12.3/scala/collection/Seq.html]]
+      */
     def +     (that: Tuple2[Int,T]*): Unit =
         if( !that.isEmpty ) {
             this(that.head)
             this + (that.tail: _*)
         }
-    /**Insere um valor no final da lista*/
+    /**
+      * Adds a value at this' tail
+      * @example {{{
+      *      var list: List[Int]
+      *      list(1,2,3)         //list = [1,2,3]
+      *      list + 4            //list = [1,2,3,4]
+      * }}}
+      * @see [[List!.insert(index:Int,value:T):Unit* this.insert(Int,T)]]
+      */
     def +    (value: T) = this((this.size,value))
-    /**Insere uma sequência de valores na Lista, argumentos na forma de value:T*/
+    /**
+      * Inserts a value sequence in this
+      * @example {{{
+      *      var list: List[Int]
+      *      list(1,2,3)         //list = [1,2,3]
+      * }}}
+      * @see [[List!.+(value:T)* this + (T)]] [[https://www.scala-lang.org/api/2.12.3/scala/collection/Seq.html]]
+      */
     def apply(that: T*): Unit = {
         if( !that.isEmpty ) { this + (that.head); this(that.tail: _*) }
     }
-
-    /**Busca e remove um valor da Lista, argumentos na forma de target:T*/
-    def - (target: T): Unit	= this.find(target) match {
-            case Some(index) => this.remove(index)
-            case None => {}
-        }
-/* PROTÓTIPO QUE RECEBE ÍNDICES
+    /**
+      * Removes a value from this
+      * @param index value's position within this
+      * @example {{{
+      *      var list: List[Int]
+      *      list(10,20,30)         //list = [10,20,30]
+      *      list - 1               //list = [10,30]
+      * }}}
+      * @see [[List!.remove(index:Int)* this.remove(Int)]]
+      */
     def - (index: Int): Unit = this.remove(index)
-*/
-    /**Busca e remove uma sequência de valores da Lista*/
+    /**
+      * Removes a value sequence from this
+      * @note It will search for the values and only remove the ones that exist within this
+      * @example {{{
+      *      var list: List[Int]
+      *      list(1,2,3,4,5)     //list = [1,2,3,4,5]
+      *      list - (1,2,5)      //list = [3,4]
+      * 
+      * }}}
+      * @see [[List!.-(index:Int)* this - (Int)]] [[https://www.scala-lang.org/api/2.12.3/scala/collection/Seq.html]]
+      */
     def - (that: T*): Unit = {
-        if( !that.isEmpty ){ this - (that.head); this - (that.tail: _*) }
+        if( !that.isEmpty ){ 
+            this.find(that.head) match { 
+                case Some(index) => this - (index)
+                case None => {}
+            }
+            this - (that.tail: _*)
+        }
     }
 }
